b719fe764b307da68f73e3fbd04713009b67b638
Uttam Patel <uttam11.velani11@gmail.com>
2020-07-23 17:18:07 +0530

    fix_db_error

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
