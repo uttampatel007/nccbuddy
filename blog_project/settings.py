dcf4538529a4c7f47d63001e236d3ef18527cf0a
Uttam Patel <uttam11.velani11@gmail.com>
2020-08-02 11:21:39 +0530

    postgres-addes

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nccbuddies',
+        'USER': 'nccbuddies_admin',
+        'PASSWORD': 'admin@nccbuddies123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
